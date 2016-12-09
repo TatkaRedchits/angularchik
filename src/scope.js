c16a06d419ab0dc8487ec1821809fa6227badfb1
Tatiana Redchits <7_tatka_7@mail.ru>
2016-12-09 14:28:24 +0200

    Executed test 1-7

--- src/scope.js
+++ src/scope.js
@@ -1,4 +1,38 @@
 function Scope() {
+    this.$watchers = [];
 }
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+    this.$watchers.push({
+        value: 'init',
+        watchFn: watchFn,
+        listenerFn: listenerFn || function() { }
+    });
+};
+
+Scope.prototype.$digest = function() {
+
+    for (var i = this.$watchers.length - 1;  i >= 0; i--) {
+
+        var watcher = this.$watchers[i];
+
+        var newValue = watcher.watchFn(this);
+
+        var oldValue = watcher.value;
+
+        if (watcher.value == 'init') {
+            watcher.value = watcher.watchFn(this);
+            watcher.listenerFn(newValue, oldValue, this);
+        }
+
+        if (watcher.value != newValue) {
+            watcher.listenerFn(newValue, oldValue, this);
+        }
+
+        watcher.value = newValue; 
+
+    }
+
+};
 
 module.exports = Scope;
